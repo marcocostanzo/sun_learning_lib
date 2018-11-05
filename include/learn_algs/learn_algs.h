fc03dba3ec4cbf608cf722307daa8129fc7ca7da
Marco Costanzo <marco.costanzo@unicampania.it>
2018-11-05 11:55:16 +0100

    using boost::function

--- include/learn_algs/learn_algs.h
+++ include/learn_algs/learn_algs.h
@@ -25,11 +25,10 @@
 #define LEARN_ALGS_LIB_H
 
 #include <cmath>
+#include <boost/function.hpp>
 
-typedef double (*FZero_FCN)(double);
+double findZero(double initial_point, const boost::function<double(double)>& Jcst, const boost::function<double(double)>& gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER );
 
-double findZero(double initial_point, FZero_FCN Jcst, FZero_FCN gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER );
-
-double findZero(double initial_point, FZero_FCN Jcst, FZero_FCN gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER, bool& b_max_iter );
+double findZero(double initial_point, const boost::function<double(double)>& Jcst, const boost::function<double(double)>& gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER, bool& b_max_iter );
 
 #endif
--- src/learn_algs/learn_algs.cpp
+++ src/learn_algs/learn_algs.cpp
@@ -24,7 +24,7 @@
 #include "learn_algs/learn_algs.h"
 
 
-double findZero(double initial_point, FZero_FCN Jcst, FZero_FCN gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER, bool& b_max_iter ){
+double findZero(double initial_point, const boost::function<double(double)>& Jcst, const boost::function<double(double)>& gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER, bool& b_max_iter ){
 
     double JJ = Jcst(initial_point);
 	bool converge = false;
@@ -51,7 +51,7 @@ double findZero(double initial_point, FZero_FCN Jcst, FZero_FCN gradJ, double GD
     return x;
 }
 
-double findZero(double initial_point, FZero_FCN Jcst, FZero_FCN gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER ){
+double findZero(double initial_point, const boost::function<double(double)>& Jcst, const boost::function<double(double)>& gradJ, double GD_GAIN, double GD_COST_TOL, double lambda, double MAX_GD_ITER ){
     bool b_max_iter;
     return findZero(initial_point, Jcst, gradJ, GD_GAIN, GD_COST_TOL, lambda, MAX_GD_ITER, b_max_iter );
 }
